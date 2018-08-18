d25bdcde27c0b10b0a5e1bae584af438bb16e46f
IvanMollov <ivan.xero.mollov@gmail.com>
2018-08-18 13:22:25 +0300

    Material UI Base

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import Button from '@material-ui/core/Button';
+
 class Article extends Component {
   render() {
     return (
       <div className="Article">
+        <Button color="primary" onClick={this.handleClose}>
+          OK
+        </Button>
         <header>
           <h2>{this.props.heading}</h2>
         </header>
